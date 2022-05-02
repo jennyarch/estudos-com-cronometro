7f479b7104324a3530bfc46a34444410439f69e0
Jennyfer C Santos <33470911+jennyarch@users.noreply.github.com>
2022-05-02 19:18:42 -0300

    Meu Primeiro Commit

---
+++ src/components/lista/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './lista.module.scss';
+import Item from './item';
+import { ITarefa } from '../../types/tarefa';
+
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}//Para adicionar o paramentro via Props
+
+function Lista({ tarefas, selecionaTarefa }: Props){
+  
+  return(
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item) => (
+        <Item 
+          selecionaTarefa={selecionaTarefa}
+          key={item.id}
+          {...item}
+        /> 
+        ))}
+      </ul>
+    </aside>
+  )
+}
+export default Lista;
